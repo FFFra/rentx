de0d81101e287924dc644e6252b82e9b3d8762e6
Flávio Fraschetti <frasqueti@gmail.com>
2023-02-25 17:56:02 +0000

    setup app theme

--- App.tsx
+++ App.tsx
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { View } from 'react-native';
 import { Home } from './src/screens/Home';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+import * as SplashScreen from 'expo-splash-screen';
+
+import { ThemeProvider } from 'styled-components';
+import theme from './src/screens/Home/styles/theme';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+
+  return (
+    <ThemeProvider theme={theme}>
+    <View onLayout={onLayoutRootView} style={{flex: 1}}>
+      <Home />
+    </View>
+    </ThemeProvider>
+  )
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -7,7 +7,6 @@ export function Home(){
   return(
     <Container>
       <Title>HOME</Title>
-
     </Container>
   );
 

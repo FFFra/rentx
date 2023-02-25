b07041196ed91a5b71a8eac3657101686f2e39ac
Flávio Fraschetti <frasqueti@gmail.com>
2023-02-25 22:09:37 +0000

    add header, svg and prettier configs

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import React, { useCallback } from 'react';
-import { View } from 'react-native';
-import { Home } from './src/screens/Home';
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_500Medium
-} from '@expo-google-fonts/inter';
 import {
   Archivo_400Regular,
   Archivo_500Medium,
-  Archivo_600SemiBold
+  Archivo_600SemiBold,
 } from '@expo-google-fonts/archivo';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  useFonts,
+} from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen';
-
+import React, { useCallback } from 'react';
+import { View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+
+import { Home } from './src/screens/Home';
 import theme from './src/screens/Home/styles/theme';
 
 SplashScreen.preventAutoHideAsync();
@@ -24,22 +24,20 @@ export default function App() {
     Inter_500Medium,
     Archivo_400Regular,
     Archivo_500Medium,
-    Archivo_600SemiBold
+    Archivo_600SemiBold,
   });
 
   const onLayoutRootView = useCallback(async () => {
-    
     if (fontsLoaded) {
       await SplashScreen.hideAsync();
     }
   }, [fontsLoaded]);
 
-
   return (
     <ThemeProvider theme={theme}>
-    <View onLayout={onLayoutRootView} style={{flex: 1}}>
-      <Home />
-    </View>
+      <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+        <Home />
+      </View>
     </ThemeProvider>
-  )
+  );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { Container, Header, TotalCars, HeaderContent } from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import { Container, Title } from './styles';
+import Logo from '../../assets/logo.svg';
 
-
-export function Home(){
-  return(
+export function Home() {
+  return (
     <Container>
-      <Title>HOME</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars> 12 cars available</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
-
 }

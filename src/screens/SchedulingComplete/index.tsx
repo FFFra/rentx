ec29406bb8a9ecca10dcbff4f2c265ce75606b19
Flávio Fraschetti <frasqueti@gmail.com>
2023-03-06 18:05:41 +0000

    scheduleComplete screen

---
+++ src/screens/SchedulingComplete/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useWindowDimensions } from 'react-native';
+
+import LogoSvg from '../../assets/logo_background_gray.svg';
+import DoneSvg from '../../assets/done.svg';
+
+import { Container, Content, Title, Message } from './styles';
+
+export function SchedulingComplete() {
+  const { width } = useWindowDimensions();
+
+  return (
+    <Container>
+      <LogoSvg width={width} />
+      <Content>
+        <DoneSvg width={80} height={80} />
+        <Title>Your car has been rented!</Title>
+        <Message>Enjoy your trip!</Message>
+      </Content>
+    </Container>
+  );
+}
